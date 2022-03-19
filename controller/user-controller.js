747e63a62a6b18e00d4d86de0a44f39b57d2cf63
Yudy Valentino <13517128@std.stei.itb.ac.id>
2022-03-19 19:21:00 +0700

    implement jwt token

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -5,20 +5,56 @@ const Redis = require('../config/redis');
 const { uuid } = require('uuidv4');
 const raw = require('body-parser/lib/types/raw');
 var jwt = require('jsonwebtoken');
+const { request } = require('chai');
 
-async function generateToken(data){
+async function generateToken(data) {
     const token = await jwt.sign(data, process.env.JWT_SECRET);
-    await Redis.set(`user-sessionj/${data.uuid}`, JSON.stringify({ token, ...data }), 'EX', 60 * 60 );
+    await Redis.set(`user-session/${data.uuid}`, JSON.stringify({ token, ...data }), 'EX', 60 * 60);
     return token;
 }
 
-async function getUsers(req,res) {
+async function verifyJwt(req, res, next) {
+    const header = req.headers.authorization;
+    if (header && header.startsWith('Bearer ')) {
+        // tokens
+        const token = header.slice(7, header.length);
+        const secret = process.env.JWT_SECRET;
+        try {
+            const decodedToken = await jwt.verify(token, secret);
+            const [[errSession, foundSession], [errExpire]] = await Redis.pipeline()
+                .get(`user-session/${decodedToken.uuid}`)
+                .expire(`user-session/${decodedToken.uuid}`, 60 * 60)
+                .exec();
+            if (errExpire) {
+                console.log(errExpire.message);
+                throw errExpire;
+            }
+            if (errSession) {
+                console.log(errSession.message);
+                throw errSession;
+            }
+            if (foundSession && JSON.parse(foundSession).token === token) {
+                console.log(foundSession)
+                next();
+            } else {
+                throw new Error('Session not found');
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ message: `Authentication failed! ${error.message}` }).end();
+        }
+    } else {
+        res.status(400).json({ message: 'Missing Authentication Token' }).end();
+    }
+}
+
+async function getUsers(req, res) {
     const listUser = await Users.getAll();
     console.log(listUser);
     res.status(200).json(listUser);
 }
 
-async function login(req, res, next){
+async function login(req, res, next) {
     const mobileNumber = req.body.mobileNumber;
     try {
         const user = await Users.findOne({
@@ -28,54 +64,54 @@ async function login(req, res, next){
         })
         let token = null;
         console.log(user);
-        if(!user){
+        if (!user) {
             const userUUID = uuid();
-            await Users.create({mobileNumber, uuid: userUUID});
-            token = await generateToken({uuid: userUUID});
+            await Users.create({ mobileNumber, uuid: userUUID });
+            token = await generateToken({ uuid: userUUID });
             res.status(201).json(token);
-        }else{
-            token = await generateToken({uuid: user.uuid});
+        } else {
+            token = await generateToken({ uuid: user.uuid });
             res.status(200).json(token).end()
         }
     } catch (error) {
-        next(error)   
+        next(error)
     }
 }
 
-async function updateUser(req,res, next){
+async function updateUser(req, res, next) {
     const userUUID = req.params.uuid;
     try {
-        const {nama, dob, city} = req.body;
+        const { nama, dob, city } = req.body;
         const user = await Users.findOne({
             where: {
                 uuid: userUUID
             },
             raw: true
         })
-        if(user){
+        if (user) {
             await Users.update({
                 nama,
                 dob,
                 city
-            },{
+            }, {
                 where: {
                     uuid: user.uuid
                 },
                 logging: true
             });
-            next({userId: user.id});
-        }else{
+            next({ userId: user.id });
+        } else {
             res.status(400).end();
-        }    
+        }
     } catch (error) {
         next(error);
     }
 }
 
-async function updateAccount(user,req,res,next){
+async function updateAccount(user, req, res, next) {
     try {
-        const {sourceOfFund, puposeAccount, occupation, avgMonthlyIncome} = req.body;
-        const {userId} = user;
+        const { sourceOfFund, puposeAccount, occupation, avgMonthlyIncome } = req.body;
+        const { userId } = user;
         await Account.create({
             userId,
             sourceOfFund,
@@ -91,8 +127,8 @@ async function updateAccount(user,req,res,next){
 
 
 async function deleteUser(req, res) {
-   const userUUID = await req.params.uuid;
-   try {
+    const userUUID = await req.params.uuid;
+    try {
 
         const user = await Users.findOne({
             where: {
@@ -100,7 +136,7 @@ async function deleteUser(req, res) {
             }
         })
 
-        if(user){
+        if (user) {
             await user.destroy({
                 where: {
                     uuid: user.uuid
@@ -108,11 +144,11 @@ async function deleteUser(req, res) {
             })
             res.status(200).end();
         }
-        res.status(400).end();    
-   } catch (error) {
-       next(error);
-   }
+        res.status(400).end();
+    } catch (error) {
+        next(error);
+    }
 }
 
 
-module.exports = {getUsers, login, updateUser, deleteUser, updateAccount};
+module.exports = { verifyJwt, getUsers, login, updateUser, deleteUser, updateAccount };
