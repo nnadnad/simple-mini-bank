e94fe76c3a291fe0242b1f0befcb28d06019c601
Yudy Valentino <13517128@std.stei.itb.ac.id>
2022-03-20 16:56:43 +0700

    add validation api

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const bodyParser = require('body-parser');
 const expressHealthcheck = require('express-healthcheck');
 const { mini_bank } = require('./config/db');
 const app = express();
+const createLogger = require('./utils/logger');
 const port = process.env.PORT || 3010;
 const {users_route} = require('./router')
+const { schemaUserUpdate } = require('./utils/joi-schema/user-schema');
+const validator = require('express-joi-validation').createValidator({
+    passError: true
+});
 
+const logger = createLogger('index');
       
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -28,11 +34,22 @@ app.use('/healthcheck', expressHealthcheck({
     },
 }));
 
-app.use('/users', users_route);
-
+app.use('/users', validator.body(schemaUserUpdate), users_route);
+app.use((err, req, res, next) => {
+  if (err && err.error && err.error.isJoi) {
+    // we had a joi error, let's return a custom 400 json response
+    res.status(400).json({
+      type: err.type, // will be "query" here, but could be "headers", "body", or "params"
+      message: err.error.toString()
+    });
+  } else {
+    // pass on to another error handler
+    next(err);
+  }
+});
 
 const server = app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  logger.info(`Example app listening on port ${port}`)
 })
 
 module.exports = server
